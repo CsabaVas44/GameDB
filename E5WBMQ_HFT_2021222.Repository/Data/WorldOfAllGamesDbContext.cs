9bd5116dfc4c116361825d2e17e246c5dd9d6fca
Vas Csaba <vascsaba5544@gmail.com>
2022-10-24 01:18:48 +0200

    dbContext added for creating dbsets later.

---
+++ E5WBMQ_HFT_2021222.Repository/Data/WorldOfAllGamesDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E5WBMQ_HFT_2021222.Repository.Data
+{
+    public class WorldOfAllGamesDbContext : DbContext
+    {
+    }
+}
