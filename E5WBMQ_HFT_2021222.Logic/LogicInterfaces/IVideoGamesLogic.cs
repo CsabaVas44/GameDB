50cebe457d36be6884df787b0d72388e73480a47
Vas Csaba <vascsaba5544@gmail.com>
2022-10-28 01:09:48 +0200

    MODIFY non-cruds extract to IVideoGamesLogic

--- E5WBMQ_HFT_2021222.Logic/LogicInterfaces/IVideoGamesLogic.cs
+++ E5WBMQ_HFT_2021222.Logic/LogicInterfaces/IVideoGamesLogic.cs
@@ -4,10 +4,15 @@ namespace E5WBMQ_HFT_2021222.Logic.LogicInterfaces
 {
     public interface IVideoGamesLogic
     {
+        double AverageSoldCopiesByPublisher(int id);
+        IQueryable<VideoGamesLogic.PublisherData> CopiesSoldByEachPublisher();
         void Create(VideoGames item);
         void Delete(int id);
+        string MostPopularGenre();
+        Publishers OldestGameReleasedByPublisher();
         VideoGames Read(int id);
         IQueryable<VideoGames> ReadAll();
+        double SoldCopiesOfGivenGenre(string name);
         void Update(VideoGames item);
     }
 }
