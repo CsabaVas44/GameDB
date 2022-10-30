2c9ddba202de1c6a7934b3031bc6c20dcdaf326f
Vas Csaba <vascsaba5544@gmail.com>
2022-10-30 18:15:51 +0100

    MODIFY Update to not overwrite virtual properties

--- E5WBMQ_HFT_2021222.Repository/ModelRepositories/PublishersRepository.cs
+++ E5WBMQ_HFT_2021222.Repository/ModelRepositories/PublishersRepository.cs
@@ -25,7 +25,10 @@ namespace E5WBMQ_HFT_2021222.Repository.ModelRepositories
             var old = Read(item.PublisherId);
             foreach (var prop in old.GetType().GetProperties())
             {
-                prop.SetValue(old, prop.GetValue(item));
+                if (prop.GetAccessors().FirstOrDefault(t => t.IsVirtual) == null)
+                {
+                    prop.SetValue(old, prop.GetValue(item));
+                }
             }
             ctx.SaveChanges();
         }
