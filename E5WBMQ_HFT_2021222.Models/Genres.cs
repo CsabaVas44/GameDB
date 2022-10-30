7ee8c59d582a20a9d8b050a9c1c458c4577f77e4
Vas Csaba <vascsaba5544@gmail.com>
2022-10-30 18:03:46 +0100

    ADD using System.Text.Json.Serialization to Genres

--- E5WBMQ_HFT_2021222.Models/Genres.cs
+++ E5WBMQ_HFT_2021222.Models/Genres.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace E5WBMQ_HFT_2021222.Models
