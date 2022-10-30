f4046c76da9baea2acca85678916fa8cb97dea02
Vas Csaba <vascsaba5544@gmail.com>
2022-10-30 18:10:30 +0100

    ADD genresController. ADD CRUDs to genresController in Endpoint.Controllers

---
+++ E5WBMQ_HFT_20212222.Endpoint/Controllers/GenresController.cs
@@ -0,0 +1,48 @@
+﻿using E5WBMQ_HFT_2021222.Logic.Logics;
+using E5WBMQ_HFT_2021222.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E5WBMQ_HFT_20212222.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        IGenresLogic logic;
+        public GenresController(IGenresLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Genres> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+
+        public Genres Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Genres value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Update([FromBody] Genres value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
