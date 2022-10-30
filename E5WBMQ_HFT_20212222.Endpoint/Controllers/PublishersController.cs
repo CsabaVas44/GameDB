651826841cdc13414dd5d093c4b639c65ff371db
Vas Csaba <vascsaba5544@gmail.com>
2022-10-30 18:11:32 +0100

    ADD PublishersController to Endpoint. ADD CRUDs to PublishersController in Endpoint.Controllers

---
+++ E5WBMQ_HFT_20212222.Endpoint/Controllers/PublishersController.cs
@@ -0,0 +1,48 @@
+﻿using E5WBMQ_HFT_2021222.Logic.Logics;
+using E5WBMQ_HFT_2021222.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E5WBMQ_HFT_20212222.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PublishersController : ControllerBase
+    {
+        IPublishersLogic logic;
+        public PublishersController(IPublishersLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Publishers> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+
+        public Publishers Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Publishers value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Update([FromBody] Publishers value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
