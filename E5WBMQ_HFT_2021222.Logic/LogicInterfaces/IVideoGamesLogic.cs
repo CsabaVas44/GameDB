fcf92f4756e6a9acbb582c59d83ce42c207cdb7c
Vas Csaba <vascsaba5544@gmail.com>
2022-11-16 20:51:51 +0100

    ADD new non-cruds

--- E5WBMQ_HFT_2021222.Logic/LogicInterfaces/IVideoGamesLogic.cs
+++ E5WBMQ_HFT_2021222.Logic/LogicInterfaces/IVideoGamesLogic.cs
@@ -4,15 +4,19 @@ namespace E5WBMQ_HFT_2021222.Logic.Logics
 {
     public interface IVideoGamesLogic
     {
-        double AverageSoldCopiesByPublisher(int id);
-        IQueryable<VideoGamesLogic.PublisherData> CopiesSoldByEachPublisher();
+        double AverageSoldCopiesByPublisher(string name);
+        IEnumerable<KeyValuePair<string, double>> CopiesSoldByEachPublisher();
+
         void Create(VideoGames item);
         void Delete(int id);
         string MostPopularGenre();
-        Publishers OldestGameReleasedByPublisher();
+        string OldestGameReleasedByWhom();
         VideoGames Read(int id);
         IQueryable<VideoGames> ReadAll();
         double SoldCopiesOfGivenGenre(string name);
         void Update(VideoGames item);
+        IQueryable<VideoGames> AllOfTheSameGenre(string genre);
+        List<string> GenrePerGame(string gameName);
+        IEnumerable<KeyValuePair<string, int>> NumberOfGamesPerGenre();
     }
 }
